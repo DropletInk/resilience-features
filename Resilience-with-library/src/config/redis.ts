6a2f4a02446cff54f6a7cd99a216115187977cfc
Palash Sarkar <palash.sarkar@droplet.ink>
2026-02-10 15:00:47 +0530

    Implemented retry handler using p-retry library

---
+++ Resilience-with-library/src/config/redis.ts
@@ -0,0 +1,25 @@
+import { createClient } from "redis";
+import dotenv from "dotenv";
+dotenv.config();
+const client = createClient({
+  username: "default",
+  // password: process.env.REDIS_PASS,
+  socket: {
+    host: process.env.REDIS_HOST || "localhost",
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+client.on("error", (err) => {
+  console.error("Redis client error", err);
+});
+
+client.on("connect", () => {
+  console.log("Connecting to redis...");
+});
+
+client.on("ready", () => {
+  console.log("Successfully connected to redis");
+});
+
+export default client;
---
+++ Resilience-with-library/src/middleware/retryHandler.ts
@@ -0,0 +1,38 @@
+import pRetry from "p-retry";
+
+export const retryHandler = async (
+  fn: () => Promise<any>,
+  maxRetry: number = 3,
+  iniDelay: number = 1000,
+  exBackoffMultiplier: number = 2,
+) => {
+  try {
+    const result = await pRetry(
+      async () => {
+        return await fn();
+      },
+      {
+        retries: maxRetry,
+        factor: exBackoffMultiplier,
+        minTimeout: iniDelay,
+        onFailedAttempt: (error) => {
+          if (error.retriesLeft === 0) {
+            console.log("Retry limit exceeded");
+            return;
+          }
+
+          const delay =
+            iniDelay * exBackoffMultiplier ** (error.attemptNumber - 1);
+          console.log(
+            `Attempt failed. Retrying in ${Math.round(delay / 1000)}s`,
+          );
+        },
+      },
+    );
+    // console.log("Success");
+    return result;
+  } catch (error) {
+    console.log("Request failed after retries");
+    throw Error;
+  }
+};
