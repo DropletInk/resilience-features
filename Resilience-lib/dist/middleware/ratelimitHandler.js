cc9b1eb446afbd7391a320b72dbe66c6f6d55088
Palash Sarkar <palash.sarkar@droplet.ink>
2026-02-24 11:17:36 +0530

    Updated dist folder

--- Resilience-lib/dist/middleware/ratelimitHandler.js
+++ Resilience-lib/dist/middleware/ratelimitHandler.js
@@ -1,19 +1,21 @@
 import { RateLimiterRedis } from "rate-limiter-flexible";
-export const rateLimitHandler = ({ client, maxRequests, durationInSec = 60 }) => async (req, res, next) => {
-    try {
-        const rateLimiter = new RateLimiterRedis({
-            storeClient: client,
-            points: maxRequests,
-            duration: durationInSec,
-        });
-        const res = await rateLimiter.consume(req.ip);
-        console.log("IP:", req.ip);
-        console.log(`Request passed ${res.consumedPoints} times`);
-        next();
-    }
-    catch (error) {
-        // console.error("Rate Limit:", error);
-        console.log("Rate limit exceeded");
-        res.status(429).json({ message: "Too many requests" });
-    }
+export const rateLimitHandler = ({ client, maxRequests, durationInSec = 60, }) => {
+    const rateLimiter = new RateLimiterRedis({
+        storeClient: client,
+        points: maxRequests,
+        duration: durationInSec,
+    });
+    return async (req, res, next) => {
+        try {
+            const result = await rateLimiter.consume(req.ip);
+            console.log("IP:", req.ip);
+            console.log(`Request passed ${result.consumedPoints} times`);
+            next();
+        }
+        catch (error) {
+            // console.error("Rate Limit:", error);
+            console.log("Rate limit exceeded");
+            res.status(429).json({ message: "Too many requests" });
+        }
+    };
 };
